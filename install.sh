33cd02ba26823ce1eff74dce8d42d69fb2c69d9e
Ehsan-work <e.zandi@m3connect.de>
2021-04-06 02:40:07 +0200

    i3 patch for online training

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 clear
-ln -sf /home/priv/git/mydwm/wmbash/ /home/priv/.config
-    files='alacritty/ sway-configs/sway/ sway-configs/waybar/ i3-configs/i3/ i3-configs/polybar/ wmbash/'
+files='alacritty/ sway-configs/sway/ sway-configs/waybar/ i3-configs/i3/ i3-configs/polybar/ wmbash/'
 currentfolder=${PWD}
 for f in $files; do 
     #echo "$currentfolder/$f $HOME/.config"
@@ -12,5 +11,5 @@ files='.vim/ .vimrc .bashrc'
 currentfolder=${PWD}
 for f in $files; do 
     #echo "$currentfolder/$f $HOME/.config"
-    ln -sf "$currentfolder/$f" "$HOME"/.config/
+    ln -sf "$currentfolder/$f" "$HOME"/
 done
