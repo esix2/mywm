47cceea07faaaa03127c7a3163fd4b16c97a6a8b
Ehsan-work <e.zandi@gmail.com>
2021-04-07 23:23:53 +0200

    xinitrc

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 clear
-files='alacritty/ sway-configs/sway/ sway-configs/waybar/ i3-configs/i3/ i3-configs/polybar/ wmbash/'
+files='alacritty/ sway-configs/sway/ sway-configs/waybar/ i3-configs/i3/ i3-configs/polybar/ wmbash/ user-dirs.dirs'
 currentfolder=${PWD}
 for f in $files; do 
     #echo "$currentfolder/$f $HOME/.config"
     ln -sf "$currentfolder/$f" "$HOME"/.config/
 done
 
-files='.vim/ .vimrc .bashrc'
+files='.vim/ .vimrc .bashrc .xinitrc'
 currentfolder=${PWD}
 for f in $files; do 
     #echo "$currentfolder/$f $HOME/.config"
