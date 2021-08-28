abecf3709588ebfca2bfd04839598546a2e877b4
Ehsan <e.zandi@gmail.com>
2021-08-28 11:15:56 +0200

    .gitconfig file added

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 clear
 files='alacritty/ i3/ polybar/ qutebrowser/ sway/ user-dirs.dirs waybar/ wmbash/'
 currentfolder=${PWD}/.config
-for f in $(ls -F .config); do 
+for f in $(ls -F .config); do
     #echo "$currentfolder/$f $HOME/.config"
     ln -sf "$currentfolder/$f" "$HOME"/.config/
 done
 
-files='bin/ .bashrc .vim/ .vimrc .xinitrc'
+files='bin/ .bashrc .gitconfig .vim/ .vimrc .xinitrc'
 currentfolder=${PWD}
-for f in $files; do 
+for f in $files; do
     #echo "$currentfolder/$f $HOME/.config"
     ln -sf "$currentfolder/$f" "$HOME"/
 done
